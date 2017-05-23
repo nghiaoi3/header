db39ab35fcccdea299a603f4bb609adac56ad0ff
le nghia <tro.ngnghia@yahoo.com.vn>
2017-05-23 06:59:40 +0000

    ssss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express()
 var PORT = process.env.PORT ||	3000;
-import {  getIP, getOS, getLang } from 'getInfo';
+import {  getIP, getOS, getLang } from './getInfo';
 
 app.get('/api/whoami', function (req,res) {
     var json =     {
